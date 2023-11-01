27b2361e69be655752d50d26f73dcd9f162553c9
sugar_zhengjie <2421275302@qq.com>
2023-11-01 17:01:20 +0800

    防重复提交使用redisson aroundByRedisson

--- src/main/java/com/zj/aspect/AntiReplayAspect.java
+++ src/main/java/com/zj/aspect/AntiReplayAspect.java
@@ -9,6 +9,8 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.integration.redis.util.RedisLockRegistry;
@@ -41,6 +43,9 @@ public class AntiReplayAspect {
 
     @Autowired
     private RedisLockRegistry redisLockRegistry;
+
+    @Autowired
+    RedissonClient redissonClient;
 
     @Pointcut("@within(antiReplay)||@annotation(antiReplay)")
     public void pointcut(AntiReplay antiReplay) {
@@ -87,6 +92,61 @@ public class AntiReplayAspect {
                     // 解锁
                     lock.unlock();
                     log.info("释放锁 success, key = [{}]", key);
+                }
+            } else {
+                // 获取锁失败，认为是重复提交的请求
+                log.info("获取锁失败 fail, key = [{}]", key);
+                //直接返回前端数据
+                sendResponse(response);
+            }
+        }else{
+            result = proceedingJoinPoint.proceed();
+        }
+        return result;
+
+    }
+
+    public Object aroundByRedisson(ProceedingJoinPoint proceedingJoinPoint, AntiReplay antiReplay) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        int tryLockTime = antiReplay.tryLockTime();
+        String path = request.getRequestURI();
+        HttpServletResponse response = attributes.getResponse();
+        Object result = null;
+        String name = antiReplay.key();
+        //User user = ContextHolder.getContext().getUser();
+        //实际项目中通过 Secure工具类获取或者请求头
+        User user = new User();
+        user.setId("1111111");
+        user.setName("zj");
+        user.setEmail("123456");
+        if(StringUtil.isEmpty(name)){
+            if (null != user && StringUtil.isNotEmpty(user.getEmail())) {
+                name = user.getName()+user.getId();
+            } else {
+                //获取nginx转发之后的真实ip
+                name = request.getHeader(XREALIP);
+            }
+        }
+        String key = name + '-' + path;
+        // 开关打开
+        if(!OFF.equalsIgnoreCase(aSwitch)){
+            //获取分布式锁
+            RLock lock = redissonClient.getLock(key);
+            long s = System.currentTimeMillis();
+            // tryLockTime 默认为0 不会等待锁变为可用，如果锁不可用，它将立即返回false，一个非阻塞的锁获取方式
+            boolean isSuccess = lock.tryLock(tryLockTime, TimeUnit.MILLISECONDS);
+            log.info("获取锁时间：[{}]", System.currentTimeMillis() - s);
+            if (isSuccess) {
+                log.info("获取锁 success, key = [{}ms]", key);
+                // 获取锁成功, 执行进程
+                try {
+                    result = proceedingJoinPoint.proceed();
+                } finally {
+                    if (lock.isLocked() && lock.isHeldByCurrentThread()) {
+                        lock.unlock();
+                        log.info("释放锁 success, key = [{}]", key);
+                    }
                 }
             } else {
                 // 获取锁失败，认为是重复提交的请求
